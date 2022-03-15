7cfb61d18c59e252af0698a1eed5ee8394adc1d8
Vladimir <1337vovalenovo@gmail.com>
2022-03-16 00:49:49 +0300

    first commit

--- app.js
+++ app.js
@@ -81,194 +81,126 @@
 // })
 //Number 3
 
-// let row = document.querySelector('.row')
-
-// fetch('https://restcountries.com/v3.1/all')
-// .then((response) => response.json())
-// .then((data) => {
-//     console.log(data)
-//     getInfo(data);
-// })
-
-// function getInfo(data){
-//     for(let i = 0;i <6;i++){
-        
-//         let imgCard = data[i+11].flags.png
-//         let title = data[i+11].name.official
-//         let loca= data[i+11].region
-//         let popul = data[i+11].population
-//         let lang = renderLangs(data[i+11].languages);
-//         let cur = renderCurrencies(data[i+11].currencies);
-//         renderCard(imgCard,title,loca,popul,lang,cur)
-//         console.log(lang,cur)
-//     }
-// }
-
-// function renderCard(imgCard,title,loca,popul,lang,cur){
-//     let card = document.createElement('div')
-//     card.className = 'card';
-//     let img = document.createElement('img');
-//     let tit = document.createElement('p');
-//     let loc = document.createElement('p');
-//     let pop = document.createElement('p');
-//     img.setAttribute('src',imgCard);
-//     tit.innerText = title;
-//     loc.innerText = loca;
-//     pop.innerText = popul;
-//     card.appendChild(img);
-//     card.appendChild(tit);
-//     card.appendChild(loc);
-//     card.appendChild(pop);
-//     card.appendChild(lang);
-//     card.appendChild(cur);
-//     row.appendChild(card);
-// }
-
-
-// function renderLangs(langs){
-//     console.log(langs)
-//     let langElement = document.createElement('p');
-//     langElement.className = 'lang-text';
-//     for(let lang in langs){
-//         let spanLang = document.createElement('span');
-//         spanLang.innerText =`${langs[lang]}`;
-//         langElement.appendChild(spanLang)
-//     }
-//     return langElement;
-// }
-
-
-// function renderCurrencies(currencies){
-//     let currencElem = document.createElement('p');
-//     currencElem.className = 'curren-text';
-//     for(let curren in currencies){
-//         let spanCurren = document.createElement('span');
-//         spanCurren.innerText = `${currencies[curren].symbol}, ${currencies[curren].name}`;
-//         currencElem.appendChild(spanCurren)
-//     }
-//     return currencElem;
-    
-// }
 //Number 4
-// let row = document.querySelector('.row');
-// let inputVal = document.querySelector('input')
-// let button = document.querySelector('button');
+let row = document.querySelector('.row');
+let inputVal = document.querySelector('input')
+let button = document.querySelector('button');
 
 
 
-// button.addEventListener('click',function(event){
-// fetch(`https://restcountries.com/v3.1/name/${inputVal.value}`)
-// .then((response) => response.json())
-// .then((data) => {
-//     let borders = data[0].borders
-//     fetch(`https://restcountries.com/v3.1/all`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         console.log(borders)
-//         searchBorder(data,borders)
-//     })
-//     getInfo(data)
-// })
-// function searchBorder(data,borders){
-//     for(let i = 0;i < borders.length;i++){
-//         const adults = data.filter(dat => {
-//             if(borders[i] == dat.cca3){
-//                 return true
-//             }
-//         })
-//         createBorder(adults)
-//     }
-// }
+button.addEventListener('click',function(event){
+fetch(`https://restcountries.com/v3.1/name/${inputVal.value}`)
+.then((response) => response.json())
+.then((data) => {
+    let borders = data[0].borders
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(borders)
+        searchBorder(data,borders)
+    })
+    getInfo(data)
+})
+function searchBorder(data,borders){
+    for(let i = 0;i < borders.length;i++){
+        const adults = data.filter(dat => {
+            if(borders[i] == dat.cca3){
+                return true
+            }
+        })
+        createBorder(adults)
+    }
+}
 
-// function createBorder(data){
-//     let imgSrc = data[0].flags.svg
-//     let title = data[0].name.official
-//     let loca= data[0].region
-//     let popul = data[0].population
-//     let lang = renderLangs(data[0].languages);
-//     let cur = renderCurrencies(data[0].currencies);
-//     renderBorder(imgSrc,title,loca,popul,lang,cur)
-// }
+function createBorder(data){
+    let imgSrc = data[0].flags.svg
+    let title = data[0].name.official
+    let loca= data[0].region
+    let popul = data[0].population
+    let lang = renderLangs(data[0].languages);
+    let cur = renderCurrencies(data[0].currencies);
+    renderBorder(imgSrc,title,loca,popul,lang,cur)
+}
 
-// function renderBorder(imgCard,title,loca,popul,lang,cur){
-//     let card = document.createElement('div')
-//     card.className = 'card';
-//     let divBorder = document.createElement('div');
-//     let textBorder = document.createElement('p')
-//     let img = document.createElement('img');
-//     let tit = document.createElement('p');
-//     let loc = document.createElement('p');
-//     let pop = document.createElement('p');
-//     textBorder.innerText = 'Соседняя страна';
-//     img.setAttribute('src',imgCard);
-//     tit.innerText = title;
-//     loc.innerText = loca;
-//     pop.innerText = popul;
-//     divBorder.className = 'border'
-//     divBorder.appendChild(textBorder)
-//     card.appendChild(divBorder);
-//     card.appendChild(img);
-//     card.appendChild(tit);
-//     card.appendChild(loc);
-//     card.appendChild(pop);
-//     card.appendChild(lang);
-//     card.appendChild(cur);
-//     row.appendChild(card);
-// }
-
-
-// function getInfo(data){
-//     let imgSrc = data[0].flags.svg
-//     let title = data[0].name.official
-//     let loca= data[0].region
-//     let popul = data[0].population
-//     let lang = renderLangs(data[0].languages);
-//     let cur = renderCurrencies(data[0].currencies);
-//     renderCard(imgSrc,title,loca,popul,lang,cur)
-// }
-// function renderCard(imgCard,title,loca,popul,lang,cur){
-//     let card = document.createElement('div')
-//     card.className = 'card';
-//     let img = document.createElement('img');
-//     let tit = document.createElement('p');
-//     let loc = document.createElement('p');
-//     let pop = document.createElement('p');
-//     img.setAttribute('src',imgCard);
-//     tit.innerText = title;
-//     loc.innerText = loca;
-//     pop.innerText = popul;
-//     card.appendChild(img);
-//     card.appendChild(tit);
-//     card.appendChild(loc);
-//     card.appendChild(pop);
-//     card.appendChild(lang);
-//     card.appendChild(cur);
-//     row.appendChild(card);
-// }
-// function renderLangs(langs){
-//     let langElement = document.createElement('p');
-//     langElement.className = 'lang-text';
-//     for(let lang in langs){
-//         let spanLang = document.createElement('span');
-//         spanLang.innerText =`${langs[lang]}`;
-//         langElement.appendChild(spanLang)
-//     }
-//     return langElement;
-// }
+function renderBorder(imgCard,title,loca,popul,lang,cur){
+    let card = document.createElement('div')
+    card.className = 'card';
+    let divBorder = document.createElement('div');
+    let textBorder = document.createElement('p')
+    let img = document.createElement('img');
+    let tit = document.createElement('p');
+    let loc = document.createElement('p');
+    let pop = document.createElement('p');
+    textBorder.innerText = 'Соседняя страна';
+    img.setAttribute('src',imgCard);
+    tit.innerText = title;
+    loc.innerText = loca;
+    pop.innerText = popul;
+    divBorder.className = 'border'
+    divBorder.appendChild(textBorder)
+    card.appendChild(divBorder);
+    card.appendChild(img);
+    card.appendChild(tit);
+    card.appendChild(loc);
+    card.appendChild(pop);
+    card.appendChild(lang);
+    card.appendChild(cur);
+    row.appendChild(card);
+}
 
 
-// function renderCurrencies(currencies){
-//     let currencElem = document.createElement('p');
-//     currencElem.className = 'curren-text';
-//     for(let curren in currencies){
-//         let spanCurren = document.createElement('span');
-//         spanCurren.innerText = `${currencies[curren].symbol}, ${currencies[curren].name}`;
-//         currencElem.appendChild(spanCurren)
-//     }
-//     return currencElem;
+function getInfo(data){
+    let imgSrc = data[0].flags.svg
+    let title = data[0].name.official
+    let loca= data[0].region
+    let popul = data[0].population
+    let lang = renderLangs(data[0].languages);
+    let cur = renderCurrencies(data[0].currencies);
+    renderCard(imgSrc,title,loca,popul,lang,cur)
+}
+function renderCard(imgCard,title,loca,popul,lang,cur){
+    let card = document.createElement('div')
+    card.className = 'card';
+    let img = document.createElement('img');
+    let tit = document.createElement('p');
+    let loc = document.createElement('p');
+    let pop = document.createElement('p');
+    img.setAttribute('src',imgCard);
+    tit.innerText = title;
+    loc.innerText = loca;
+    pop.innerText = popul;
+    card.appendChild(img);
+    card.appendChild(tit);
+    card.appendChild(loc);
+    card.appendChild(pop);
+    card.appendChild(lang);
+    card.appendChild(cur);
+    row.appendChild(card);
+}
+function renderLangs(langs){
+    let langElement = document.createElement('p');
+    langElement.className = 'lang-text';
+    for(let lang in langs){
+        let spanLang = document.createElement('span');
+        spanLang.innerText =`${langs[lang]}`;
+        langElement.appendChild(spanLang)
+    }
+    return langElement;
+}
+
+
+function renderCurrencies(currencies){
+    let currencElem = document.createElement('p');
+    currencElem.className = 'curren-text';
+    for(let curren in currencies){
+        let spanCurren = document.createElement('span');
+        spanCurren.innerText = `${currencies[curren].symbol}, ${currencies[curren].name}`;
+        currencElem.appendChild(spanCurren)
+    }
+    return currencElem;
     
-// }
-// })
+}
+})
 
 
 //Number 5
